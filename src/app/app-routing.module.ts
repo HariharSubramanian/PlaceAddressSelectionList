dac5c9d6e110e39e03fa06b56c5721d6bbe58685
Harihar <hari.harihar20@gmail.com>
2023-02-24 08:57:55 +0530

    data values updation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LocationSelectorComponent } from './components/location-selector/location-selector.component';
 
 const routes: Routes = [
-  { path: '', component: LocationSelectorComponent }
+  { path: '', component: LocationSelectorComponent },
 ];
 
 @NgModule({

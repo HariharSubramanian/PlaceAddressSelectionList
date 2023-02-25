cacf4cb71f37b85bf41bc22e05dd1d7966fe1356
Harihar <hari.harihar20@gmail.com>
2023-02-25 08:19:59 +0530

    interfaces created

---
+++ src/interfaces/countries.ts
@@ -0,0 +1,16 @@
+export interface Country {
+    "name": string,
+    "isoCode": string,
+    "flag": string,
+    "phonecode": string,
+    "currency": string,
+    "latitude": string,
+    "longitude": string,
+    "timezones": {
+        "zoneName": string,
+        "gmtOffset": number,
+        "gmtOffsetName": string,
+        "abbreviation": string,
+        "tzName": string
+    }
+}
---
+++ src/interfaces/states.ts
@@ -0,0 +1,7 @@
+export interface State {
+    "name": string,
+    "isoCode": string,
+    "countryCode": string,
+    "latitude": string,
+    "longitude": string,
+}
